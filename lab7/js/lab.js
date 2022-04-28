8085b1cd9b3b54ccaab1166efb61637aa48cefcc
Pichy J <pichayapatj@gmail.com>
2022-04-27 21:53:37 -0700

    lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,25 @@
+// Authors: Pichy Jumpholwong and Zac Nakamura
+// Created: 26 April 2022
+// License: Public Domain
+
+function names() {
+  var userName = window.prompt("Enter your name! ");
+  console.log("Your name is ", userName);
+
+  //split
+  var nameSplit = userName.split(' ');
+  console.log("The name split is ", nameSplit);
+
+  // sort
+  var nameSort = nameSplit.sort();
+  console.log("The sorted name is ", nameSort);
+
+  //join
+  var nameJoin = nameSort.join('');
+  console.log("The name joined is ", nameJoin);
+
+  return nameSort;
+}
+
+document.writeln("Check out your name: ",
+  names(), "</br>");
